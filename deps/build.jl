3939830d20e4e9d1a8a9355d14777b7ca4d1ca77
Miles Lubin <miles.lubin@gmail.com>
2013-11-02 23:36:59 -0400

    add libtarget

--- deps/build.jl
+++ deps/build.jl
@@ -8,6 +8,6 @@ provides(Sources,URI("http://ab-initio.mit.edu/nlopt/nlopt-2.4.tar.gz"), libnlop
 
 provides(BuildProcess,Autotools(configure_options =
     ["--enable-shared", "--without-guile", "--without-python",
-    "--without-octave", "--without-matlab"]),libnlopt, os = :Unix)
+    "--without-octave", "--without-matlab"], libtarget="libnlopt.la"),libnlopt, os = :Unix)
 
 @BinDeps.install
