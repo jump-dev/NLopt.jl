2f310184ffe56d261045358b6e8d76d1c042601d
Miles Lubin <miles.lubin@gmail.com>
2013-11-04 23:07:19 -0500

    enable --with-cxx

--- deps/build.jl
+++ deps/build.jl
@@ -2,12 +2,13 @@ using BinDeps
 
 @BinDeps.setup
 
-libnlopt = library_dependency("libnlopt")
+libnlopt = library_dependency("libnlopt", aliases=["libnlopt_cxx"])
 
 provides(Sources,URI("http://ab-initio.mit.edu/nlopt/nlopt-2.4.tar.gz"), libnlopt)
 
 provides(BuildProcess,Autotools(configure_options =
     ["--enable-shared", "--without-guile", "--without-python",
-    "--without-octave", "--without-matlab"], libtarget="libnlopt.la"),libnlopt, os = :Unix)
+    "--without-octave", "--without-matlab","--with-cxx"],
+    libtarget="libnlopt_cxx.la"),libnlopt, os = :Unix)
 
 @BinDeps.install
