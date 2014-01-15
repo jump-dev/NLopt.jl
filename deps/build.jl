d99e5a5995c5667a0b253d99cdf5375e7a41d37a
Sébastien Villemot <sebastien@debian.org>
2014-01-15 16:39:36 +0100

    Add support for Debian package of libnlopt.

--- deps/build.jl
+++ deps/build.jl
@@ -4,6 +4,8 @@ using BinDeps
 
 libnlopt = library_dependency("libnlopt", aliases=["libnlopt_cxx", "libnlopt$(WORD_SIZE)"])
 
+provides(AptGet, "libnlopt0", libnlopt)
+
 provides(Sources,URI("http://ab-initio.mit.edu/nlopt/nlopt-2.4.tar.gz"), libnlopt)
 
 provides(BuildProcess,Autotools(configure_options =
