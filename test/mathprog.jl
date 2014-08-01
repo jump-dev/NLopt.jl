159163cab00001d68c1ab99f1fbe3c5f70b36b96
Miles Lubin <miles.lubin@gmail.com>
2014-08-01 17:37:32 -0600

    test derivative-free algorithm with MathProgBase interface

--- test/mathprog.jl
+++ test/mathprog.jl
@@ -2,3 +2,5 @@ using NLopt
 
 include(joinpath(Pkg.dir("MathProgBase"),"test","nlp.jl"))
 nlptest(NLoptSolver(algorithm=:LD_SLSQP))
+# test derivative-free
+rosenbrocktest(NLoptSolver(algorithm=:LN_PRAXIS))
