30703e44fb35f5ac7d0dd1c7983ec74c662688e2
Tony Kelman <tony@kelman.net>
2016-01-06 14:19:52 -0800

    Greatly simplify Windows BinDeps invocation
    should fix #48

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,7 @@ using BinDeps
 
 @BinDeps.setup
 
-libnlopt = library_dependency("libnlopt", aliases=["libnlopt_cxx", "libnlopt$(WORD_SIZE)"])
+libnlopt = library_dependency("libnlopt", aliases=["libnlopt_cxx", "libnlopt$(WORD_SIZE)", "libnlopt-0"])
 
 provides(AptGet, "libnlopt0", libnlopt)
 
@@ -25,32 +25,8 @@ srcdir = BinDeps.srcdir(libnlopt)
 downloadsdir = BinDeps.downloadsdir(libnlopt)
 extractdir(w) = joinpath(srcdir,"w$w")
 destw(w) = joinpath(libdir,"libnlopt$(w).dll")
-provides(BuildProcess,
-	(@build_steps begin
-		FileDownloader("http://ab-initio.mit.edu/nlopt/$(nloptname)-dll32.zip", joinpath(downloadsdir, "$(nloptname)-dll32.zip"))
-		FileDownloader("http://ab-initio.mit.edu/nlopt/$(nloptname)-dll64.zip", joinpath(downloadsdir, "$(nloptname)-dll64.zip"))
-		CreateDirectory(srcdir, true)
-		CreateDirectory(joinpath(srcdir,"w32"), true)
-		CreateDirectory(joinpath(srcdir,"w64"), true)
-		FileUnpacker(joinpath(downloadsdir,"$(nloptname)-dll32.zip"), extractdir(32), joinpath(extractdir(32),"matlab"))
-		FileUnpacker(joinpath(downloadsdir,"$(nloptname)-dll64.zip"), extractdir(64), joinpath(extractdir(64),"matlab"))
-		CreateDirectory(libdir, true)
-		@build_steps begin
-			ChangeDirectory(extractdir(32))
-			FileRule(destw(32), @build_steps begin
-				`cp libnlopt-0.dll $(destw(32))`
-				end)
-		end
-		@build_steps begin
-			ChangeDirectory(extractdir(64))
-			FileRule(destw(64), @build_steps begin
-				`cp libnlopt-0.dll $(destw(64))`
-				end)
-		end
-	end), libnlopt, os = :Windows)
 
-@windows_only push!(BinDeps.defaults, BuildProcess)
+provides(Binaries, URI("http://ab-initio.mit.edu/nlopt/$(nloptname)-dll$(WORD_SIZE).zip"),
+         libnlopt, unpacked_dir="w$WORD_SIZE", os = :Windows)
 
 @BinDeps.install Dict(:libnlopt => :libnlopt)
-
-@windows_only pop!(BinDeps.defaults)
