f689e992db1b77c9dafbb33ae9e08b881326734e
Benoît Legat <benoit.legat@gmail.com>
2020-09-29 09:21:04 +0200

    Fix MPB tests on nightly

--- test/MPB_wrapper.jl
+++ test/MPB_wrapper.jl
@@ -1,8 +1,7 @@
 using NLopt
 
-# test model from MathProgBase, using undocumented
-# function Base.find_package for now (julia#27592):
-nlp = joinpath(dirname(Base.find_package(NLopt, "MathProgBase")), "..", "test", "nlp.jl")
+import MathProgBase
+nlp = joinpath(dirname(dirname(pathof(MathProgBase))), "test", "nlp.jl")
 include(nlp)
 
 nlptest(NLoptSolver(algorithm=:LD_SLSQP))
