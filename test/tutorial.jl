09e1e7b52cdf2a928d36803e1bde50bfdec41d5c
odow <o.dowson@gmail.com>
2023-01-25 15:05:47 +1300

    Add license headers

--- test/tutorial.jl
+++ test/tutorial.jl
@@ -1,3 +1,8 @@
+# Copyright (c) 2013: Steven G. Johnson and contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 using NLopt, Test
 
 count = 0 # keep track of # function evaluations
