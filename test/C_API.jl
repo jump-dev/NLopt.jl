798cb94cfbe321dc1ab792376354248b5d65680d
Oscar Dowson <odow@users.noreply.github.com>
2025-01-06 14:47:20 +1300

    Update C_API.jl

--- test/C_API.jl
+++ test/C_API.jl
@@ -567,7 +567,11 @@ function test_algorithm_name()
     sol = "Limited-memory BFGS (L-BFGS) (local, derivative-based)"
     @test algorithm_name(algorithm) == sol
     @test algorithm_name(:LD_LBFGS) == sol
-    @test algorithm_name(10) == sol
+    if NLopt._is_version_newer_than_2_9()
+        @test algorithm_name(10) == sol
+    else
+        @test algorithm_name(11) == sol
+    end
     opt = Opt(:LD_LBFGS, 2)
     @test algorithm_name(opt) == sol
     sprint(show, algorithm_name(:LD_LBFGS))
