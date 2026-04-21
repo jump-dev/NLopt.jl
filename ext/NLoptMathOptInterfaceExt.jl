9a82f23b6f5bb806b1fb6200e8dc9bc1c1da7733
Benoît Legat <benoit.legat@gmail.com>
2026-04-21 10:35:50 +0200

    Checkout MOI bl/nonlinear_model

--- ext/NLoptMathOptInterfaceExt.jl
+++ ext/NLoptMathOptInterfaceExt.jl
@@ -31,7 +31,7 @@ mutable struct Optimizer <: MOI.AbstractOptimizer
     variables::MOI.Utilities.VariablesContainer{Float64}
     starting_values::Vector{Union{Nothing,Float64}}
     nlp_data::MOI.NLPBlockData
-    nlp_model::Any  # created by MOI.Nonlinear.nonlinear_model(ad_backend)
+    nlp_model::Any  # created by MOI.Nonlinear.model(ad_backend)
     ad_backend::MOI.Nonlinear.AbstractAutomaticDifferentiation
     sense::Union{Nothing,MOI.OptimizationSense}
     objective::Union{
@@ -562,11 +562,8 @@ function MOI.supports(
 end
 
 function MOI.get(model::Optimizer, ::MOI.ObjectiveFunctionType)
-    if model.nlp_model !== nothing
-        obj = model.nlp_model.objective
-        if obj !== nothing
-            return MOI.ScalarNonlinearFunction
-        end
+    if model.nlp_model !== nothing && model.nlp_model.objective !== nothing
+        return MOI.ScalarNonlinearFunction
     end
     return typeof(model.objective)
 end
@@ -601,7 +598,7 @@ function _init_nlp_model(model)
         if !(model.nlp_data.evaluator isa _EmptyNLPEvaluator)
             error("Cannot mix the new and legacy nonlinear APIs")
         end
-        model.nlp_model = MOI.Nonlinear.nonlinear_model(model.ad_backend)
+        model.nlp_model = MOI.Nonlinear.model(model.ad_backend)
     end
     return
 end
